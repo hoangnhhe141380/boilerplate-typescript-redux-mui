c9507bc577c85f491343e8d133692eeca4e12f29
Hoang <hoangnh2000.dev@gmail.com>
2022-08-15 17:32:50 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@mui/material'
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant='contained'>Hi</Button>
+    </div>
+  );
+}
+
+export default App;
